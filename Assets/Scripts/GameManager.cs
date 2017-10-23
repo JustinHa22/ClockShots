6a184b875ab23537d26af9bb1b284254c7850c2a
Justin Ha <jch583@nyu.edu>
2017-10-23 16:40:46 -0400

    Changed scores from static int to PlayerPrefs for efficiency. Also commented clearly on GameManager script.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour {
 	float slowDown; 
 
 	int score; 
-	static int highScore; 
+	int highScore; 
 
 	public Text scoreText; 
 	public Text clockText; 
@@ -23,24 +23,33 @@ public class GameManager : MonoBehaviour {
 
 	public Text restartText; 
 
+	string highscoreKey = "Highscore"; 
+
 	bool restart; 
 
 	// Use this for initialization
 	void Start () {
+		//Sets the clock to 60 secs when the game starts
 		clock = 60;
+		//Score starts at 0 
 		score = 0; 
-		highScore = 0; 
+		//Turns off restart function so the game can be played
 		restart = false; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Sets the highscore to the highest score stored in PlayerPrefs
+		highScore = PlayerPrefs.GetInt (highscoreKey); 
 
+		//The clock will go down depending on how much the bullets have slowed down
 		clock -= (slowDown * Time.deltaTime); 
 
+		//Finds the gun in the scene and gets the script on it
 		GameObject gun = GameObject.Find ("Gun");
 		Gun gunscript = gun.GetComponent<Gun> ();
 
+		//This switch statement gives each bullet count an amount to slow down the clock by
 		switch (gunscript.bulletCount) {
 		case 6:
 			slowDown = 1f; 
@@ -64,37 +73,48 @@ public class GameManager : MonoBehaviour {
 			slowDown = .25f;
 			break; 
 		}
-			
+
+		//The clock will read the lowest int of the current time
 		clockText.text = "" + Mathf.FloorToInt(clock); 
 
+		//Finds the target in the scene and gets the scripts
 		GameObject target = GameObject.FindGameObjectWithTag ("Target");
 		Target targetScript = target.GetComponent<Target> ();
 
+		//If target was hit with a bullet, add 1 point
 		if (targetScript.bulletHit) {
 			score += 1; 
 		}
-
+		//If target was hit with a hitbox/melee attack, add 1 point
 		if (targetScript.hitboxHit) {
 			score += 1; 
 		}
 
-		scoreText.text = "Score: " + score; 
+		//The score will show as the score earned this round
+		scoreText.text = "Score: " + score;  
 
-		if (highScore < score) {
-			highScore = score; 
+		//If the score is greater than the highscore, than set the PlayerPrefs to the score
+		if (score > highScore) {
+			PlayerPrefs.SetInt (highscoreKey, score); 
+			PlayerPrefs.Save (); 
 		}
 
+		//If the clock reaches less than 0, Game Over
 		if (clock <= 0) {
+			clock = 0; 
 			GameOver (); 
 		}
 
+		//Finds the player on the scene and gets its script 
 		GameObject player = GameObject.FindGameObjectWithTag ("Player");
 		Movement playerScript = player.GetComponent<Movement> ();
 
+		//If player is hit with a bullet, Game Over
 		if (playerScript.playerDead) {
 			//GameOver ();
 		}
 
+		//If the restart is true and Escape is pressed, load the scene again and return Time.timeScale back to 1
 		if (restart) {
 			if (Input.GetKeyDown (KeyCode.Escape)) {
 				SceneManager.LoadScene(0);
@@ -103,6 +123,7 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
+	//Function that is called when the game is over and presents the score and highscore on the screen
 	void GameOver(){
 		Time.timeScale = 0;
 		gameOverText.text = "Game Over"; 
---
+++ Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+	Vector3 startPosition; // remember where we were before we started shaking
+	public float shakeStrength = 0f;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.localPosition;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+			shakeStrength = 3f;
+
+		// decay the shake strength
+		shakeStrength = Mathf.Clamp( shakeStrength - Time.deltaTime, 0f, 1f);
+
+//		Vector3 shakeSideDirection = transform.right * Mathf.Sin( Time.time * 27f) * 0.16f;
+//		Vector3 shakeUpDirection = transform.up * Mathf.Sin( Time.time * 23f) * 0.16f;
+//		transform.localPosition = startPosition + (shakeSideDirection + shakeUpDirection) * shakeStrength;
+
+	}
+
+	public void shake(){
+		Vector3 shakeSideDirection = transform.right * Mathf.Sin( Time.time * 27f) * 0.16f;
+		Vector3 shakeUpDirection = transform.up * Mathf.Sin( Time.time * 23f) * 0.16f;
+		transform.localPosition = startPosition + (shakeSideDirection + shakeUpDirection) * shakeStrength;
+	}
+}
